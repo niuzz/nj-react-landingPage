5e5c99baf98f62bb8d6bfc65966ffece50e317e8
niuzz <niuzz@hotmail.com>
2018-03-01 22:38:09 +0800

    clean somting

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Button from 'material-ui/Button';
 import {Link, Route} from 'react-router-dom';
@@ -8,10 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React...</h1>
-        </header>
+        </header> */}
+        <h2>clean something</h2>
         <Link to = "/setPage">
           <Button variant = "raised" color = "primary" > 此处可以点击 </Button>
         </Link>
