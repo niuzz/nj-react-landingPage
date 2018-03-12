83d4696870e6997bb1aad6b6d5af37a35502ee25
niuzz <niuzz@hotmail.com>
2018-03-12 10:59:44 +0800

    ssr init

---
+++ src/build/webpack.config.server.js
@@ -0,0 +1,5 @@
+/******************************************
+ *  Author : niuzz niuzz@hotmail.com   
+ *  Created On : Mon Mar 12 2018
+ *  File : webpack.config.server.js.js
+ *******************************************/
