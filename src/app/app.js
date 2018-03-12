15e3a81f374aae645f25861cf307dd0946368276
niuzz <niuzz@hotmail.com>
2018-03-12 11:42:25 +0800

    client

---
+++ src/app/app.js
@@ -0,0 +1,17 @@
+/******************************************
+ *  Author : niuzz niuzz@hotmail.com   
+ *  Created On : Mon Mar 12 2018
+ *  File : app.js
+ *******************************************/
+const React = require('react')
+const ReactDom = require('react-dom')
+import App from './App.jsx'
+const root = document.getElementById('root')
+
+console.log('-----------------------')
+console.log(111111)
+console.log('-----------------------')
+
+ReactDom.render(
+	<App />, root
+)
--- src/build/webpack.config.client.js
+++ src/build/webpack.config.client.js
@@ -3,3 +3,32 @@
  *  Created On : Mon Mar 12 2018
  *  File : webpack.config.client.js
  *******************************************/
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+	entry: {
+		app: path.join(__dirname, '../app/app.js')
+	},
+	output: {
+		filename: '[name].[hash].js',
+		path: path.join(__dirname, '../../dist')
+	},
+	module: {
+		rules: [
+			{
+				test: /.js$/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /.jsx$/,
+				loader: 'babel-loader'
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, '../app/template.html')
+		})
+	]
+}
