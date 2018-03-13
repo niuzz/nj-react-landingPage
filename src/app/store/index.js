39f100f6a330d4f00e0aa120303677145704cd52
niuzz <niuzz@hotmail.com>
2018-03-14 07:08:11 +0800

    create dir

---
+++ src/app/store/index.js
@@ -0,0 +1,5 @@
+/** ****************************************
+ *  Author : niuzz niuzz@hotmail.com
+ *  Created On : Wed Mar 14 2018
+ *  File : index.js
+ ****************************************** */
