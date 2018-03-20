1ec01c66ed43a66ef2280a61fe0cf79594b99233
niuzz <niuzz@hotmail.com>
2018-03-20 23:55:23 +0800

    add component layout 404 document.ejs etc

---
+++ src/components/GlobalHeader/index.js
@@ -0,0 +1,5 @@
+/******************************************
+ *  Author : niuzz niuzz@hotmail.com   
+ *  Created On : Tue Mar 20 2018
+ *  File : index.js
+ *******************************************/
---
+++ src/layouts/index.js
@@ -0,0 +1,12 @@
+/******************************************
+ *  Author : niuzz niuzz@hotmail.com   
+ *  Created On : Tue Mar 20 2018
+ *  File : index.js
+ *******************************************/
+import MainLayout from 'components/MainLayout';
+
+export default function (props) {
+	return (
+		<MainLayout />
+	);
+}
