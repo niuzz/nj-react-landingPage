c6c590dbdf2ef6aba071a629d9c94d5e3eb7058a
niuzz <niuzz@hotmail.com>
2018-03-20 23:13:56 +0800

    add umi to master

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+/******************************************
+ *  Author : niuzz niuzz@hotmail.com
+ *  Created On : Tue Mar 20 2018
+ *  File : index.js
+ *******************************************/
+export default () => <div>Index Page</div>
