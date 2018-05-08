23119f95e46c1eaa2a82d7bcde774abc756f9d16
niuzz <niuzz@hotmail.com>
2018-05-08 14:00:34 +0800

    14.0 create-react-app initial

--- nt-app/src/container/admin/admin-login/index.js
+++
@@ -1,5 +0,0 @@
-/******************************************
- *  Author : niuzz niuzz@hotmail.com   
- *  Created On : Tue May 08 2018
- *  File : index.js
- *******************************************/
--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-/******************************************
- *  Author : niuzz niuzz@hotmail.com   
- *  Created On : Sat Apr 28 2018
- *  File : index.js
- *******************************************/
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from './redux/configureStore';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import App from './containers/App';
-
-import 'normalize.css';
-import 'element-theme-default';
-
-const store = configureStore();
-
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
