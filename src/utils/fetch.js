d619c483cdc683f1574175b72c70e6d6a05f053c
niuzz <niuzz@hotmail.com>
2018-05-10 10:04:05 +0800

    gitee init

---
+++ src/utils/fetch.js
@@ -0,0 +1,5 @@
+/******************************************
+ *  Author : niuzz niuzz@hotmail.com   
+ *  Created On : Wed May 09 2018
+ *  File : fetch.js
+ *******************************************/
